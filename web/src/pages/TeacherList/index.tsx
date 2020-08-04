189700e75a94de147187466a2f47a93aa00470f9
lenilsondc <lenilson.decastro@yahoo.com>
2020-08-03 22:33:42 -0300

    feat(web): create TeacherList page layout

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,34 @@
 import React from "react";
 
+import "./styles.css";
+
+import PageHeader from "../../components/PageHeader";
+import TeacherItem from "../../components/TeacherItem";
+
 const TeacherList: React.FC = () => (
-  <div>
-    <h1>Teachers</h1>
+  <div id="page-teacher-list" className="container">
+    <PageHeader title="These are the available proffys">
+      <form id="search-teachers">
+        <div className="input-block">
+          <label htmlFor="subject">Subject</label>
+          <input type="text" id="subject" />
+        </div>
+        <div className="input-block">
+          <label htmlFor="weekday">Weekday</label>
+          <input type="text" id="weekday" />
+        </div>
+        <div className="input-block">
+          <label htmlFor="time">Time</label>
+          <input type="text" id="time" />
+        </div>
+      </form>
+    </PageHeader>
+
+    <main>
+      <TeacherItem />
+      <TeacherItem />
+      <TeacherItem />
+    </main>
   </div>
 );
 
