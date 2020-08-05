29f80abe6b2349d8c66ee7fff69560f32e6aaf21
lenilsondc <lenilson.decastro@yahoo.com>
2020-08-04 21:37:21 -0300

    chore(server): fix column default value

--- server/src/database/migrations/20200804193442_create_connections.ts
+++ server/src/database/migrations/20200804193442_create_connections.ts
@@ -3,7 +3,10 @@ import * as Knex from "knex";
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("connections", (table) => {
     table.increments("id").primary();
-    table.timestamp("created_at").defaultTo("now()").notNullable();
+    table
+      .timestamp("created_at")
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP"))
+      .notNullable();
 
     table
       .integer("user_id")
