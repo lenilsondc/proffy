adeb5431d51d35214e65317a540ee7bc3bdcfa2a
lenilsondc <lenilson.decastro@yahoo.com>
2020-08-05 20:17:59 -0300

    feat(web): add subtitle to PageHeader

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -8,9 +8,14 @@ import backIcon from "../../assets/images/icons/back.svg";
 
 interface PageHeaderProps {
   title: string;
+  subtitle?: string;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => (
+const PageHeader: React.FC<PageHeaderProps> = ({
+  title,
+  subtitle = "",
+  children,
+}) => (
   <header className="page-header">
     <div className="top-bar-container">
       <Link to="/">
@@ -20,6 +25,7 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => (
     </div>
     <div className="header-content">
       <strong>{title}</strong>
+      {subtitle && <p>{subtitle}</p>}
       {children}
     </div>
   </header>
