096d51705c21bdd11b651d9e745cede098a04d8b
lenilsondc <lenilson.decastro@yahoo.com>
2020-08-05 21:09:45 -0300

    refactor(web): replace raw html with components

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -5,13 +5,41 @@ import "./styles.css";
 import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
 import Input from "../../components/Input";
+import Select from "../../components/Select";
 
 const TeacherList: React.FC = () => (
   <div id="page-teacher-list" className="container">
     <PageHeader title="These are the available proffys">
       <form id="search-teachers">
-        <Input name="subject" label="Subject" />
-        <Input name="weekday" label="Weekday" />
+        <Select
+          name="subject"
+          label="Subject"
+          options={[
+            { value: "Arts", label: "Arts" },
+            { value: "Biology", label: "Biology" },
+            { value: "Sciences", label: "Sciences" },
+            { value: "Physical Education", label: "Physical Education" },
+            { value: "Physics", label: "Physics" },
+            { value: "Geography", label: "Geography" },
+            { value: "History", label: "History" },
+            { value: "English", label: "English" },
+            { value: "Chemistry", label: "Chemistry" },
+          ]}
+        />
+
+        <Select
+          name="weekday"
+          label="Weekday"
+          options={[
+            { value: "0", label: "Sunday" },
+            { value: "1", label: "Monday" },
+            { value: "2", label: "Tuesday" },
+            { value: "3", label: "Wednesday" },
+            { value: "4", label: "Thursday" },
+            { value: "5", label: "Friday" },
+            { value: "6", label: "Saturday" },
+          ]}
+        />
         <Input name="time" label="Time" type="time" />
       </form>
     </PageHeader>
