d953999dc3cc18e0b2714f385792d9ebaf7b965d
lenilsondc <lenilson.decastro@yahoo.com>
2020-08-08 14:34:42 -0300

    feat(mobile): implement favorites logic

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import { AsyncStorage } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
 
 import { Container, TeachersFlatList } from "./styles";
 import PageHeader from "../../components/PageHeader";
-import TeacherItem from "../../components/TeacherItem";
+import TeacherItem, { Teacher } from "../../components/TeacherItem";
 
 const Favorites: React.FC = () => {
+  const [favorites, setFavorites] = useState<Teacher[]>([]);
+
+  useFocusEffect(
+    useCallback(() => {
+      loadFavorites();
+    }, [])
+  );
+
+  function loadFavorites() {
+    AsyncStorage.getItem("favorites").then((value) => {
+      const favs = JSON.parse(value || "[]") as Teacher[];
+
+      setFavorites(favs);
+    });
+  }
+
   return (
     <Container>
       <PageHeader title="My favorite Proffys" />
 
       <TeachersFlatList
-        data={[1, 2, 3]}
-        keyExtractor={(item) => String(item)}
-        renderItem={({ item }) => <TeacherItem />}
+        data={favorites}
+        renderItem={({ item }) => (
+          <TeacherItem key={item.id} teacher={item} favorited />
+        )}
       />
     </Container>
   );
--- mobile/src/pages/Favorites/styles.ts
+++ mobile/src/pages/Favorites/styles.ts
@@ -6,7 +6,7 @@ export const Container = styled.View`
   background-color: #f0f0f7;
 `;
 
-export const TeachersFlatList = styled(FlatList)`
+export const TeachersFlatList = styled(FlatList as new () => FlatList<Teacher>)`
   margin-top: -40px;
   padding: 0 16px 24px 16px;
 `;
