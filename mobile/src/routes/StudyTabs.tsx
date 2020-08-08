0b72bfc5d847de0f477abcf6088bd8b9cdde0789
lenilsondc <lenilson.decastro@yahoo.com>
2020-08-08 09:30:54 -0300

    fix(mobile): set focused icon color

--- mobile/src/routes/StudyTabs.tsx
+++ mobile/src/routes/StudyTabs.tsx
@@ -41,8 +41,12 @@ const StudyTabs: React.FC = () => (
       component={TeacherList}
       options={{
         tabBarLabel: "Proffys",
-        tabBarIcon: ({ color, size }) => (
-          <Ionicons name="ios-easel" size={size} color={color} />
+        tabBarIcon: ({ color, size, focused }) => (
+          <Ionicons
+            name="ios-easel"
+            size={size}
+            color={focused ? "#8257e5" : color}
+          />
         ),
       }}
     ></Screen>
@@ -51,8 +55,12 @@ const StudyTabs: React.FC = () => (
       component={Favorites}
       options={{
         tabBarLabel: "Favorites",
-        tabBarIcon: ({ color, size }) => (
-          <Ionicons name="ios-heart" size={size} color={color} />
+        tabBarIcon: ({ color, size, focused }) => (
+          <Ionicons
+            name="ios-heart"
+            size={size}
+            color={focused ? "#8257e5" : color}
+          />
         ),
       }}
     ></Screen>
