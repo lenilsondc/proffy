212c9f4aaedec267720d2223551596c665732e57
lenilsondc <lenilson.decastro@yahoo.com>
2020-08-08 13:30:32 -0300

    feat(mobile): make FlatList typed

--- mobile/src/pages/TeacherList/styles.ts
+++ mobile/src/pages/TeacherList/styles.ts
@@ -2,12 +2,14 @@ import styled from "styled-components/native";
 import { FlatList } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
 
+import { Teacher } from "../../components/TeacherItem";
+
 export const Container = styled.View`
   flex: 1;
   background-color: #f0f0f7;
 `;
 
-export const TeachersFlatList = styled(FlatList)`
+export const TeachersFlatList = styled(FlatList as new () => FlatList<Teacher>)`
   margin-top: -40px;
   padding: 0 16px 24px 16px;
 `;
