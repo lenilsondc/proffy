f460a298926cca97a0e3f9c0336af850db88eb21
lenilsondc <lenilson.decastro@yahoo.com>
2020-08-08 09:02:55 -0300

    feat(mobile): create TeacherList page

---
+++ mobile/src/pages/TeacherList/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "react-native";
+
+import { Container } from "./styles";
+
+const TeacherList: React.FC = () => {
+  return (
+    <Container>
+      <Text>Teacher List</Text>
+    </Container>
+  );
+};
+
+export default TeacherList;
