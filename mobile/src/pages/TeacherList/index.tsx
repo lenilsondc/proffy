aa591b90a0618776bac2e76c21c405d6a770c680
lenilsondc <lenilson.decastro@yahoo.com>
2020-08-08 12:28:48 -0300

    feat(mobile): create TeacherList page layout

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,12 +1,70 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { Feather } from "@expo/vector-icons";
 
-import { Container } from "./styles";
+import {
+  Container,
+  TeachersFlatList,
+  SearchForm,
+  Label,
+  Input,
+  InputGroup,
+  InputBlock,
+  SubmitButton,
+  SubmitButtonText,
+} from "./styles";
+import PageHeader from "../../components/PageHeader";
+import TeacherItem from "../../components/TeacherItem";
+import { BorderlessButton } from "react-native-gesture-handler";
 
 const TeacherList: React.FC = () => {
+  const [showFilter, setShowFilter] = useState(false);
+
+  function toggleShowFilter() {
+    setShowFilter(!showFilter);
+  }
+
   return (
     <Container>
-      <Text>Teacher List</Text>
+      <PageHeader
+        title="Available Proffys"
+        headerRight={
+          <BorderlessButton style={{ padding: 8 }} onPress={toggleShowFilter}>
+            <Feather
+              name="filter"
+              size={20}
+              color={showFilter ? "#d4c2ff" : "#fff"}
+            />
+          </BorderlessButton>
+        }
+      >
+        {showFilter && (
+          <SearchForm>
+            <Label>Subject</Label>
+            <Input placeholder="Subject" />
+
+            <InputGroup>
+              <InputBlock>
+                <Label>Weekday</Label>
+                <Input placeholder="Weekday" />
+              </InputBlock>
+              <InputBlock>
+                <Label>Time</Label>
+                <Input placeholder="Time" />
+              </InputBlock>
+            </InputGroup>
+
+            <SubmitButton>
+              <SubmitButtonText>Search</SubmitButtonText>
+            </SubmitButton>
+          </SearchForm>
+        )}
+      </PageHeader>
+
+      <TeachersFlatList
+        data={[1, 2, 3]}
+        keyExtractor={(item) => String(item)}
+        renderItem={({ item }) => <TeacherItem />}
+      />
     </Container>
   );
 };
--- mobile/src/pages/TeacherList/styles.ts
+++ mobile/src/pages/TeacherList/styles.ts
@@ -1,6 +1,58 @@
 import styled from "styled-components/native";
+import { FlatList } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
 
 export const Container = styled.View`
   flex: 1;
   background-color: #f0f0f7;
+`;
+
+export const TeachersFlatList = styled(FlatList)`
+  margin-top: -40px;
+  padding: 0 16px 24px 16px;
+`;
+
+export const SearchForm = styled.View`
+  margin-bottom: 24px;
+`;
+
+export const Label = styled.Text`
+  color: #d4c2ff;
+  font-family: Poppins_400Regular;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: "#c1bccc",
+})`
+  height: 54px;
+  background-color: #fff;
+  border-radius: 8px;
+  justify-content: center;
+  padding-right: 16px;
+  padding-left: 16px;
+  margin-top: 4px;
+  margin-bottom: 16px;
+`;
+
+export const InputGroup = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const InputBlock = styled.View`
+  width: 48%;
+`;
+
+export const SubmitButton = styled(RectButton)`
+  background-color: #04d361;
+  height: 56px;
+  border-radius: 8px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SubmitButtonText = styled.Text`
+  color: #fff;
+  font-family: Archivo_700Bold;
+  font-size: 16px;
 `;
